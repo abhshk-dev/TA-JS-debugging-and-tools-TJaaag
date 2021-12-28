94fa7aa2d55dc0f850c94d014dc347353f8cd695
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2021-12-28 20:23:36 +0530

    Block-2

--- block-BJaafo/code/script.js
+++ block-BJaafo/code/script.js
@@ -24,7 +24,7 @@ function totalAmount(amount,taxRate){
     return amount+(amount*taxRate);
 }
 
-let result=totalAmount(10,0.5);
+let result=totalAmount(100,0.5);
 
 let expectedSum=150;
 console.log(result);
@@ -33,4 +33,9 @@ if(result !== expectedSum){
 }
 
 
-result=totalAmount()
+result=totalAmount(50,0.5);
+expectedSum=75;
+console.log(result);
+if(result !== expectedSum){
+    throw new Error(`${result} is not same as ${expectedSum}`);
+}
